b430c59fa4fb604a7ea8288630f6f9df545581fb
zhengwen1 <3111274609@qq.com>
2020-12-01 17:08:38 +0800

    123456

---
+++ student_pro/src/com/student/model/StudentModel.java
@@ -0,0 +1,12 @@
+package com.student.model;
+
+public class StudentModel {
+
+	private int id;
+	private String username;
+	private String password;
+	private int sex;
+	private String love;
+	
+	
+}
